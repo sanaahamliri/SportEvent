b3d3f65b1365aa46139d4f78f38e744fc9fc276b
sanaahamliri <sanahamlirifr01@gmail.com>
2024-11-29 20:00:06 +0100

    chore: Remove deprecated MongoDB options useNewUrlParser and useUnifiedTopology

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
