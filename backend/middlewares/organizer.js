49186f5527a5b222507c111c089fb0563738b801
sanaahamliri <sanahamlirifr01@gmail.com>
2024-11-29 19:26:02 +0100

    feat: Add Dockerfile for backend

--- backend/middlewares/organizer.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/authMiddleware');
-
-router.get('/organizer', auth, (req, res) => {
-  res.json({ msg: 'Bienvenue sur la page des organisateurs' });
-});
-
-module.exports = router;
