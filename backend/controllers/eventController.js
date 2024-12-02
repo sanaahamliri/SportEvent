49526506ea303006203fffca42fe1339966cc4c6
sanaahamliri <sanahamlirifr01@gmail.com>
2024-12-03 00:28:04 +0100

    add GetEventById method

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -19,6 +19,16 @@ exports.getEvents = async (req, res) => {
   }
 };
 
+exports.getEventById = async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+    if (!event) return res.status(404).json({ message: 'Event not found' });
+    res.status(200).json(event);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching event by ID', error });
+  }
+};
+
 exports.updateEvent = async (req, res) => {
   try {
     const updatedEvent = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
